94be1e1a45db7e951d808b532a1fdc0547e69b80
yuhaidong <ryang2007@outlook.com>
2018-06-21 13:50:34 +0800

    可测试JwfToken的小程序

---
+++ src/test/java/com/yhd/project/JwtTokenTest2/JwtToken.java
@@ -0,0 +1,84 @@
+package com.yhd.project.JwtTokenTest2;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+
+
+public class JwtToken {
+
+	/**
+	 * 公钥密码，保存在服务器，客户端是不会知道密码的，以防止被攻击
+	 */
+	public static String SECRET = "FreeMaNong";
+	
+	/**
+	 * 生成Token
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static String createToken() throws Exception{
+		
+		// 签发时间
+		Date iatDate = new Date();
+		
+		// 设置过期时间 - 设置签发时间1分钟后为延迟时间
+		Calendar nowTime = Calendar.getInstance();
+		nowTime.add(Calendar.MINUTE, 1);
+		// 得到过期时间
+		Date expirensDate = nowTime.getTime();
+		
+		// 组合header
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("alg", "HS256");
+		map.put("typ", "JWT");
+		
+		String token = JWT.create()
+				.withHeader(map)					//header
+				.withClaim("name", "Free码农")		//payload
+				.withClaim("age", "28")
+				.withClaim("org", "今日头条")
+				.withExpiresAt(expirensDate)		//设置过期时间，过期时间要大于签发时间
+				.withIssuedAt(iatDate)				//设置签发时间
+				.sign(Algorithm.HMAC256(SECRET));	//加密
+		
+		return token;
+	}
+	
+	/**
+	 * 解密Token
+	 * 
+	 * @param token
+	 * @return
+	 * @throws Exception
+	 */
+	public static Map<String, Claim> verifyToken(String token) throws Exception {
+		
+		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET))
+				.build();
+		
+		DecodedJWT jwt = null;
+		
+		try {
+			jwt = verifier.verify(token);
+		} catch (Exception e) {
+			// TODO: handle exception
+//			throw new RuntimeException("登录认证已过期，请重新登陆");
+			e.printStackTrace();
+			
+			System.out.println("登录认证已过期，请重新登陆");
+			System.out.println("验证Token出现错误");
+		}
+		
+		return jwt.getClaims();
+	}
+}
---
+++ src/test/java/com/yhd/project/JwtTokenTest2/JwtTokenDemo.java
@@ -0,0 +1,37 @@
+package com.yhd.project.JwtTokenTest2;
+
+import java.util.Map;
+
+import com.auth0.jwt.interfaces.Claim;
+
+/**
+ * 
+ * 
+ * @author flx
+ *
+ */
+public class JwtTokenDemo {
+
+	public static void main(String[] args) throws Exception{
+		
+		// 生成Token
+		String token = JwtToken.createToken();
+		
+		// 显示Token
+		System.out.println("Token:" + token);
+		
+		// 解密Token
+		Map<String, Claim> claims = JwtToken.verifyToken(token);
+		
+		System.out.println(claims.get("name").asString());
+		System.out.println(claims.get("age").asString());
+		System.out.println(claims.get("org") == null ? null : claims.get("org").asString());
+	
+		// 使用过期后的Token进行校验
+		String tokenExpire = "22";
+		
+		Map<String, Claim> claimsExpire = JwtToken.verifyToken(tokenExpire);
+		
+		System.out.println("测试结束");
+	}
+}
