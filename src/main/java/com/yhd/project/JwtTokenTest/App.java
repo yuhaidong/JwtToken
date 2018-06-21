7a2df1b49a5574ea80769f89abdb48ee25b4ecb3
yuhaidong <ryang2007@outlook.com>
2018-06-21 09:57:58 +0800

    JwfToken的使用测试

---
+++ src/main/java/com/yhd/project/JwtTokenTest/App.java
@@ -0,0 +1,13 @@
+package com.yhd.project.JwtTokenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
